0060debd9d41cba9574b07d3d036a082892ef813
fulsel <logan.fulse10@gmail.com>
2014-09-16 08:42:55 -0400

    Worked on #1

---
+++ Looping/Looping/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Looping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int sum;
+            int num;
+            while (true)
+            {
+                Console.WriteLine("Please enter a number. ");
+                string input = Console.ReadLine();
+                num = Convert.ToInt32(input);
+                sum = num 
+
+        }
+    }
+}
